0d90dd21eb13a8995758ecd5b68ba005f908bb2b
gnimdoubodjona <gnimdoubodjona@gmail.com>
2025-12-01 04:51:58 +0100

    Petite amélioration de l'ui du modal

--- components/dashboard/Soumission.tsx
+++ components/dashboard/Soumission.tsx
@@ -129,7 +129,7 @@ export default function SoumissionModal({ participationId, challengeId }: any) {
 
       {open && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
-          <div className="bg-white p-6 rounded-lg max-w-md w-full max-h-[90vh] overflow-y-auto">
+          <div className="bg-black p-6 rounded-lg max-w-md w-full max-h-[90vh] overflow-y-auto">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-xl font-bold">Nouvelle Soumission</h2>
               <button 
